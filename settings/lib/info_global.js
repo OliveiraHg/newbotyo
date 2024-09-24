9287888e51a44ecc309ef2832d67a9f64cff216d
Your Name <you@example.com>
2024-09-24 13:31:14 -0300

    first commit

---
+++ settings/lib/info_global.js
@@ -0,0 +1,101 @@
+exports.listLanguages = (prefix) => {
+return `Você pode ver a lista de idiomas disponíveis em para usar no tradutor ou gtts aqui: https://cloud.google.com/translate/docs/languages
+–
+• Exemplificando o uso das funções dos comandos *gtts e tradutor*:
+–
+↳ *Comando:* ${prefix}gtts pt sabrina
+*[pt]* - O idioma, o sotaque que ele(a) irá falar no áudio.
+*[sabrina]* - Vai ser o que será o texto ou frase falado por ele(a).
+–
+↳ *Comando:* ${prefix}tradutor pt|love you
+*[pt]* - O idioma que vai ser realizado a tradução da palavra ou texto.
+*[love you]* - O texto/palavra que ele(a) vai fazer a tradução para o idioma.`
+}
+
+exports.bemvindo = (prefix) => {
+return `Este comando é para informar, como você deve ativar o bem vindo, e lembrando, esse bemvindo, contém uma legenda diferente, a cada grupo que for colocado, o bemvindo2 tem essa função também.
+–
+• As diferenças entre os bem vindos é que o bemvindo tem foto e o bemvindo2 não contém foto em si somente a mensagem.
+–
+• Para desativar ou ativar o *bemvindo ou bemvindo2*, use os números 0 ou 1. 
+    • Exemplo de como ativar e desativar o recurso em seu grupo: 
+        • *Ativando o recurso:* ${prefix}bemvindo 1 ou ${prefix}bemvindo2 1, ative somente um dos dois para evitar flood em seu grupo.
+        • *Desativando o recurso:* ${prefix}bemvindo 0 ou ${prefix}bemvindo2 0, caso um dos 2 esteja ativado.
+–
+• Para realizar a troca do fundo da imagem do bem vindo, é só mandar uma foto no WhatsApp, retangular e marcar ela com o comando *${prefix}fundobemvindo* ou se quer trocar a do saiu, use *${prefix}fundosaiu*
+       • *Observação:* Os comandos para mudar o fundo é somente disponibilizado o uso ao proprietário(s) do bot.
+–
+• *Parâmetros usados que podem ser usados para personalizar sua mensagem:*
+*#hora#* => Com essa definição inserida em seu argumento, irá mostrar a hora no momento exato que foi realizada a entrada ou saída do integrante do grupo.
+*#nomedogp#* => Usada para mostrar o nome do grupo em seu texto de bem-vindo.
+*#numerodele#* => Tem a função de mostrar o número do integrante que entrou ou saiu do grupo.
+*#prefixo#* => Vai aparecer qual o símbolo que você está utilizando no bot, para fazer ele funcionar.
+*#descrição#* => Com essa definição irá mostrar a descrição do grupo ou regras como você preferir chamar.
+–
+Para definir uma legenda de quando um integrante sair ou entrar em seu grupo, veja os exemplos baixo:
+• Adicionar a legenda ou mensagem para dar as saudações ao novo integrante de forma personalizada: 
+    • Caso esteja usando o *bem-vindo* (com foto), use: *${prefix}legendabv sua mensagem..*
+    • Usando o *bem-vindo2* (sem foto)? Use *${prefix}legendabv2 sua mensagem..*
+• Definir uma legenda ou mensagem para a saída de um integrante do grupo:
+    • Caso esteja usando o *bem-vindo* (com foto), use: *${prefix}legendasaiu sua mensagem..*
+    • Usando o *bem-vindo2* (sem foto)? Use *${prefix}legendasaiu2 sua mensagem..*`
+}
+
+exports.infoOwner = (prefix, NickDono, numerodn, NomeDoBot, sender) => {
+return `Olá @${sender.split("@")[0]}, aqui está as informações sobre meu dono:
+–
+• Número do proprietário: wa.me/${numerodn}
+• Proprietário: ${NickDono}`
+}
+
+exports.tutorialBasic = (prefix) => {
+return `👤 Olá, eu sou a *Sabrina-BOT*!
+• Aqui está um breve tutorial de algumas das minhas funções:
+–
+• Para divertir seu grupo, use: *${prefix}brincadeiras* e saiba todos meus jogos.
+–
+• Para fazer figurinhas, use *${prefix}sticker* marcando um(a) foto ou vídeo.
+—
+• Para renomear o pacote ou roubar uma figurinha, ou seja, colocar autor e pacote de forma personalizada, use o comando: *${prefix}rename pacote/autor*
+–
+• Baixar fotos/vídeos de redes sociais como *Instagram, Tiktok ou Twitter, etc...* É fácil! Veja o tutorial abaixo:
+       • Baixar mídias do instagram: *${prefix}igdl [link do post/reel]*
+        • Para baixar vídeos do tiktok, use:  *${prefix}tiktok [link do vídeo]*, caso seja imagens use: *${prefix}tiktokimg [link]*
+        • Baixar publicações do Twitter, use: *${prefix}twtdl [link do post]*
+        • Ainda tenho um comando que realiza download's de 5 redes sociais ao mesmo tempo, seriam elas: *Twitter, Facebook, Instagram, Threads e Tiktok*. para realizar seu pedido, use: *${prefix}multidl [link do post]*
+–
+• Também realizo download de músicas do *YouTube e SoundCloud*. Veja:
+         • Baixar áudios em mp3 ou doc e vídeos do YouTube: *${prefix}play*, *${prefix}playvid*, *${prefix}playdoc*.
+         • Realize download de um mix de um artista, usando: *${prefix}playmix [cantor]*
+         • Para baixar músicas em mp3 no SoundCloud, use: *${prefix}scdl [link]*
+–
+Pronto, aqui foi um tutorial básico de como usar o bot. 
+• Para ver todas as minhas funções de forma detalhada, use: *${prefix}menu*`
+}
+
+exports.configbot = (prefix) => {
+return `*C​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​OMO CONFIGURAR O BOT, PRA SER DONO:*
+–
+Primeiro ligue o bot no termux, e vá para o WhatsApp do bot em algum privado, utilize os seguintes comandos.
+*Obs:* Pode por letras modificadas também se você quiser!
+–
+• *1)* Para alterar o nome do bot, use o seguinte comando:
+     • ${prefix}Nome-bot Nome que deseja colocar em seu bot
+–
+• *2)* Para alterar o nick do dono do bot, use o seguinte comando:
+     • ${prefix}Nick-dono Seu nick aqui
+–
+• *3)* Configure o número do dono, com o seguinte comando:
+     • ${prefix}numero-dono 558288279194
+*Obs:* O número tem que ser junto, e não pode conter o símbolo de + nem - e não pode ter o 9 da operadora, tem que ser o número idêntico ao seu do whatsapp.
+–
+• *4)* Configure o prefixo que você deseja no bot, usando o seguinte comando:
+     • ${prefix}prefixo-bot #
+*Obs:* Pode por qualquer símbolo, se o seu símbolo for ${prefix} ele vai passar a ser # de acordo com o que você mudou.
+–
+Boa sorte! Dúvidas? Entre em contato com o criador, usando: ${prefix}criador`
+}
+
+exports.infoConnect = () => {
+return `Texto em produção amigão! ;)`
+}
